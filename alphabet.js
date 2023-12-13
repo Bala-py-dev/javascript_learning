136a36f40a237103a649e9f9789dd270b70d0085
Bala-py-dev <bholaram.panda5297@gmail.com>
2023-12-13 19:34:29 +0530

    new changes

--- alphabet.js
+++ alphabet.js
@@ -3,13 +3,19 @@
 const alphaStr = 'ABCKWQDEPFGMHODIJKALYMNCQOPQRBSTLBUVLWXYZ'
 const newStr = []
 
-for (const alpha of alphaStr) {
+// for (const alpha of alphaStr) {
+    // if (!newStr.includes(alpha)){
+    //     newStr.push(alpha)
+    // }
+// };
+
+alphaStr.split('').forEach(alpha => {
     if (!newStr.includes(alpha)){
         newStr.push(alpha)
     }
-};
-
-console.log('1st:-', newStr.sort().join(''))
+});
+console.log(newStr)
+console.log('1st:-', newStr.join(''))
 
 const uniqueChars = [...new Set(alphaStr)].sort().join('')
 console.log('2nd:-', uniqueChars)
--- checkPassword.js
+++ checkPassword.js
@@ -1,13 +1,14 @@
 // 2. Write a code to check the user-entered password should have min length of 6 and a max length of 14 characters, the first character must be in upper case, and the password must contain lower case, upper case, numbers, and special characters.
 
 
-const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,14}$/;
-const checkPassword = (password) => {
-    if (password && passwordRegex.test(password)){
-        console.log(true)
-    } else {
-        console.log(false)
-    }
-};
+const isValidPassword = (password) => {
+    const regex = /^([A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{6,14}$/;
+    return regex.test(password);
+}
 
-checkPassword('A11@@@Aaaa');
+const userEnteredPassword = "Aaa12@12Aaa12@12Aaa12@12Aaa12@12";
+if (isValidPassword(userEnteredPassword)) {
+    console.log("Password is valid");
+} else {
+    console.log("Password is not valid");
+}
